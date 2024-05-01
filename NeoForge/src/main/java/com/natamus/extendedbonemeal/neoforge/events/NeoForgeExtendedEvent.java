513e91bf112b07472ef34746fc3e26a808c6ac4a
Rick South <rick@natamus.com>
2024-05-01 16:41:31 +0200

    Extended Bone Meal source code for 1.20.6.

---
+++ NeoForge/src/main/java/com/natamus/extendedbonemeal/neoforge/events/NeoForgeExtendedEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.extendedbonemeal.neoforge.events;
+
+import com.natamus.extendedbonemeal.events.ExtendedEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeExtendedEvent {
+	@SubscribeEvent
+	public static void onNetherwartClick(PlayerInteractEvent.RightClickBlock e) {
+		ExtendedEvent.onCropClick(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec());
+	}
+}
