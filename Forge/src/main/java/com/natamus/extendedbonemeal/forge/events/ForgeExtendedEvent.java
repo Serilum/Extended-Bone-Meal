d989f7b5e33c9ec45e531d8652c0d64bf0c74f34
Rick South <rick@natamus.com>
2025-02-12 23:34:56 +0100

    (v3.6) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/extendedbonemeal/forge/events/ForgeExtendedEvent.java
+++ Forge/src/main/java/com/natamus/extendedbonemeal/forge/events/ForgeExtendedEvent.java
@@ -3,12 +3,10 @@ package com.natamus.extendedbonemeal.forge.events;
 import com.natamus.extendedbonemeal.events.ExtendedEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeExtendedEvent {
 	@SubscribeEvent
-	public void onNetherwartClick(PlayerInteractEvent.RightClickBlock e) {
+	public static void onNetherwartClick(PlayerInteractEvent.RightClickBlock e) {
 		ExtendedEvent.onCropClick(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec());
 	}
 }
--- NeoForge/src/main/java/com/natamus/extendedbonemeal/neoforge/events/NeoForgeExtendedEvent.java
+++ NeoForge/src/main/java/com/natamus/extendedbonemeal/neoforge/events/NeoForgeExtendedEvent.java
@@ -3,9 +3,7 @@ package com.natamus.extendedbonemeal.neoforge.events;
 import com.natamus.extendedbonemeal.events.ExtendedEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeExtendedEvent {
 	@SubscribeEvent
 	public static void onNetherwartClick(PlayerInteractEvent.RightClickBlock e) {
